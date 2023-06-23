809d732175912950aa9505f633c442711fa79313
Jacob Harden <jacob.harden@hotmail.com>
2023-06-23 14:14:10 -0500

    [Update] Implemented MUI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hardened Games
         </p>
       </header>
+
+        <main>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          <Grid container spacing={4}>
+            {featuredPosts.map((post) => (
+              <FeaturedPost key={post.title} post={post} />
+            ))}
+          </Grid>
+          <Grid container spacing={5} sx={{ mt: 3 }}>
+            <Main title="From the firehose" posts={posts} />
+            <Sidebar
+              title={sidebar.title}
+              description={sidebar.description}
+              archives={sidebar.archives}
+              social={sidebar.social}
+            />
+          </Grid>
+        </main>
     </div>
   );
 }
